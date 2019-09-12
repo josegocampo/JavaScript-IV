91e148290fd711ced6e33fd2f73eb089ff93bac6
josegocampo <jose.ocampo@mail.udp.cl>
2019-09-12 19:27:43 -0300

    finalized changes to JS IV Project

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,12 +1,16 @@
+
+//CLASSES//
+
+
 class Person  {
     constructor(personAttr) {
     this.name = personAttr.name;
     this.age = personAttr.age;
     this.location = personAttr.location;
-    this.speak = function() {
-      console.log(`Hello my name is ${this.name}, I am from ${this.location}.`)
     }
-  }
+    speak() {
+      console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
 }
 
 
@@ -17,39 +21,139 @@ class Instructor extends Person {
     this.specialty = instructorAttr.specialty;
     this.favLanguage = instructorAttr.favLanguage;
     this.catchPhrase = instructorAttr.catchPhrase; 
-    this.demo = function(){
-        console.log(`Today we are learning about ${this.subject}.`)
-    };
-    this.grade = function(){
-        console.log(`${this.name} receives a perfect score on ${this.subject}`)
     }
-  }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}.`);
+    }
+    grade(student,subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+      //QUESTION 1: I can call student.name and when logging the Keiran Instructor I can link it here, not sure exactly how given that Student and Instructor are siblings, I dont realize why Instructor is having acces to the jose student, but it is.  Second part of this question is, how can I call on a specific subject out of the favorite subjects array in here, without having to do jose.fabSubjects[numberofsubject] .
+    }
 }
 
-class Student extends Person {
+
+class Student extends Instructor {
 
     constructor(studentAttr) {
         super(studentAttr);
         this.previousBackground = studentAttr.previousBackground;
         this.className = studentAttr.className;
         this.favSubjects = studentAttr.favSubjects;
-        this.listSubjects = function(){
-            console.log(this.favSubjects)
-
-        };
-        this.PRAssignment = function(){
-            console.log(`${this.name} has submited a PR for ${this.favSubject[1]}`)
-
-        };
-        this.sprintChallenge = function(){
-            console.log(`${this.name} has begun sprint challenge on ${this.favSubject[0]}`)
-        }
     }
+        listSubjects(){
+          console.log(this.favSubjects);
+        }
+        PRAssignment(subject){
+            console.log(`${this.name} has submited a PR for ${subject}`);
+        }
+        sprintChallenge(subject){
+            console.log(`${this.name} has begun sprint challenge on ${subject}`);
+        }
+}
+
+
+class ProjectManager extends Person {
+
+    constructor(ProjectManagerAttr) {
+        super(ProjectManagerAttr);
+        this.gradClassName = ProjectManagerAttr.gradClassName;
+        this.favInstructor = ProjectManagerAttr.favInstructor;
+        
+    }
+        standUp(channel){
+          console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+        }
+        debugsCode(student, subject){
+            console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+        }
 }
 
 
 
 
+//Student Objects//
+
+const jose = new Student({
+  age: 32,
+  name: 'Jose',
+  location: 'Chile',
+  previousBackground: 'Lawyer',
+  className: 'WEB PT10',
+  favSubjects: ['HTML', 'CSS', 'JavaScript'],
+  
+});
+
+
+const dylan = new Student({
+  age: 30,
+  name: 'Dylan',
+  location: 'USA',
+  previousBackground: 'Coder',
+  className: 'WEB PT10',
+  favSubjects: ['React', 'Node', 'Python'], 
+})
+
+
+//Instructor Objects//
+
+const keiran = new Instructor({
+  age: 30,
+  name: 'Keiran',
+  location: 'USA',
+  specialty: 'Teaching Code',
+  favLanguage: 'JS', 
+  catchPhrase: 'for the Horde'
+})
+
+const edward = new Instructor({
+  age: 31,
+  name: 'Edward',
+  location: 'USA',
+  specialty: 'TL SSJ',
+  favLanguage: 'Java',
+  catchPhrase: 'sure thing'
+})
+
+
+// PM Objects //
+
+const richard = new ProjectManager({
+  age: 28,
+  name: 'Richard',
+  location: 'USA',
+  gradClassName: 'WEB 20',
+  favInstructor: 'Dan',
+})
+
+const mikis = new ProjectManager({
+  age: 29,
+  name: 'Mikis',
+  location: 'USA',
+  gradClassName: 'WEB 20',
+  favInstructor: 'Joe',
+})
+
+
+console.log("----------------------------------------------------")//For separation purposes between the 2 homeworks.
+
+jose.PRAssignment(jose.favSubjects[2]);
+dylan.PRAssignment(dylan.favSubjects[1]);
+
+keiran.grade(jose, jose.favSubjects[0]);
+edward.demo(edward.favLanguage);
+
+richard.standUp("webpt10")
+mikis.debugsCode(jose, jose.favSubjects[2]);
+
+
+
+
+// #### Person
+
+// * First we need a Person class. This will be our `base-class`
+// * Person receives `name` `age` `location` all as props
+// * Person receives `speak` as a method.
+// * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
 
 // #### Instructor
 
